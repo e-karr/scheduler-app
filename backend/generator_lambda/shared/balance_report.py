719e53ff99326be7c9211d950748ed51d3744ea4
Elizabeth Karr <e.karr93@icloud.com>
2026-02-01 16:46:33 -0600

    Update balance report to use new Game data class

--- backend/generator_lambda/shared/balance_report.py
+++ backend/generator_lambda/shared/balance_report.py
@@ -1,4 +1,31 @@
-def generate_balance_report(schedule: dict[str, list[dict]], num_teams: int) -> dict:
+from dataclasses import dataclass
+from shared.game import Game
+
+
+@dataclass
+class BalanceReport:
+    """
+    Data class representing the final season Balance Report
+
+    :param season_avg_diff: The average rank difference in matchups for the whole season
+    :type season_avg_diff: float
+    :param week_diff_dict: A dictionary of the week by week average mathcup rank difference
+    :type week_diff_dict: dict[str, float]
+    :param parity_score: Ratio representing how closely matched the season matchups are
+    :type parity_score: float
+    :param quality_status: Status for how balanced the season is
+    :type quality_status: str
+    """
+
+    season_avg_diff: float
+    week_diff_dict: dict[str, float]
+    parity_score: float
+    quality_status: str
+
+
+def generate_balance_report(
+    schedule: dict[str, list[Game]], num_teams: int
+) -> BalanceReport:
     """
     Generates the balance report based on passed season schedule and number of teams.
 
@@ -6,113 +33,51 @@ def generate_balance_report(schedule: dict[str, list[dict]], num_teams: int) -> 
     ```python
         {
             "Week 1": [
-                {
-                    "rank_a": 3,
-                    "name_a": "Global Cafe",
-                    "rank_b": 4,
-                    "name_b": "Invictus Fitness",
-                    "diff": 1,
-                    "weight": 9,
-                },
-                {
-                    "rank_a": 6,
-                    "name_a": "Lawrence Shirt Factory",
-                    "rank_b": 10,
-                    "name_b": "Sacred Sword",
-                    "diff": 4,
-                    "weight": 6,
-                },
-                {
-                    "rank_a": 9,
-                    "name_a": "Free State",
-                    "rank_b": 8,
-                    "name_b": "Honey Badgers",
-                    "diff": 1,
-                    "weight": 9,
-                },
-                {
-                    "rank_a": 7,
-                    "name_a": "JDE",
-                    "rank_b": 5,
-                    "name_b": "Red Legs",
-                    "diff": 2,
-                    "weight": 8,
-                },
-                {
-                    "rank_a": 1,
-                    "name_a": "Johnny’s",
-                    "rank_b": 2,
-                    "name_b": "Post Haus",
-                    "diff": 1,
-                    "weight": 9,
-                },
+                Game(Team('Global Cafe', 3), Team('Invictus Fitness', 4), 9),
+                Game(Team('LSF', 6), Team('Sacred Sword', 10), 6),
+                Game(Team('Free State', 9), Team('Honey Badgers', 8), 9),
+                Game(Team('JDE', 7), Team('Red Legs', 5), 8),
+                Game(Team('Johnny’s', 1), Team('Post Haus', 2), 9)
             ],
             "Week 2": [
-                {
-                    "rank_a": 3,
-                    "name_a": "Global Cafe",
-                    "rank_b": 5,
-                    "name_b": "Red Legs",
-                    "diff": 2,
-                    "weight": 8,
-                },
-                {
-                    "rank_a": 7,
-                    "name_a": "JDE",
-                    "rank_b": 6,
-                    "name_b": "Lawrence Shirt Factory",
-                    "diff": 1,
-                    "weight": 9,
-                },
-                {
-                    "rank_a": 4,
-                    "name_a": "Invictus Fitness",
-                    "rank_b": 2,
-                    "name_b": "Post Haus",
-                    "diff": 2,
-                    "weight": 8,
-                },
-                {
-                    "rank_a": 9,
-                    "name_a": "Free State",
-                    "rank_b": 1,
-                    "name_b": "Johnny’s",
-                    "diff": 8,
-                    "weight": 2,
-                },
-                {
-                    "rank_a": 8,
-                    "name_a": "Honey Badgers",
-                    "rank_b": 10,
-                    "name_b": "Sacred Sword",
-                    "diff": 2,
-                    "weight": 8,
-                },
+                Game(Team('Global Cafe', 3), Team('Red Legs', 5), 8),
+                Game(Team('JDE', 7), Team('LSF', 6), 9),
+                Game(Team('Invictus Fitness', 4), Team('Post Haus', 2), 8),
+                Game(Team('Free State', 9), Team('Johnny’s', 1), 2),
+                Game(Team('Honey Badgers', 8), Team('Sacred Sword', 10), 8)
             ]
         }
     ```
 
     Example balance report:
     ```python
-        {
-            "season_avg_diff": 2.4,
-            "week_diffs_dict": {"Week 1": 1.8, "Week 2": 3, "Week 3": 1.4, "Week 4": 3.4},
-            "parity_score": 0.27,
-            "status": "GOOD: Balanced matchups. The weighting is clearly working.",
-        }
+        BalanceReport(
+            2.4,
+            {
+                "Week 1": 1.8,
+                "Week 2": 3,
+                "Week 3": 1.4,
+                "Week 4": 3.4
+            },
+            0.27,
+            "GOOD"
+        )
     ```
 
     :param schedule: Full generated season schedule
-    :type schedule: dict[str, list[dict]]
+    :type schedule: dict[str, list[Game]]
     :param num_teams: Total number of teams
     :type num_teams: int
-    :return: Description
-    :rtype: dict
+    :return: The season balance report detailing the season average rank differance, the week by week average rank difference, parity score, and balance status
+    :rtype: BalanceReport
     """
 
-    week_diffs_dict = _calc_weekly_averages(schedule)
+    week_diffs_dict = {
+        week: _calc_weekly_average(games) for week, games in schedule.items()
+    }
+
     total_diff = sum(
-        game["diff"] for week_games in schedule.values() for game in week_games
+        game.rank_diff for week_games in schedule.values() for game in week_games
     )
     total_games = sum(len(games) for _, games in schedule.items())
     max_possible_diff = num_teams - 1
@@ -123,14 +88,7 @@ def generate_balance_report(schedule: dict[str, list[dict]], num_teams: int) -> 
 
     status = _determine_status(quality_ratio)
 
-    balance_report = {
-        "season_avg_diff": season_avg_diff,
-        "week_diffs_dict": week_diffs_dict,
-        "parity_score": quality_ratio,
-        "status": status,
-    }
-
-    return balance_report
+    return BalanceReport(season_avg_diff, week_diffs_dict, quality_ratio, status)
 
 
 def _determine_status(quality_ratio: float) -> str:
@@ -149,42 +107,29 @@ def _determine_status(quality_ratio: float) -> str:
     :rtype: str
     """
     if quality_ratio <= 0.25:
-        status = "EXCELLENT: Highly competitive. Teams are playing very close seeds."
+        status = "EXCELLENT"
     elif quality_ratio <= 0.40:
-        status = "GOOD: Balanced matchups. The weighting is clearly working."
+        status = "GOOD"
     elif quality_ratio <= 0.55:
-        status = "MODERATE: Fairly spread out. Close to a standard random draw."
+        status = "MODERATE"
     else:
-        status = "LOW: High rank disparity. Consider increasing your weight exponent."
+        status = "LOW"
 
     return status
 
 
-def _calc_weekly_averages(schedule: dict[str, list[dict]]) -> dict[str, float]:
+def _calc_weekly_average(games: list[Game]) -> float:
     """
-    Calculates the average rank difference for matchups per week. Creates a dictionary with the per week average rank difference
+    Calculates the average rank difference for Games in a week.
 
-    Example:
-    ```python
-        {
-            "Week 1": 1.8,
-            "Week 2": 3,
-            "Week 3": 1.4,
-            "Week 4": 3.4
-        }
-    ```
-
-    :param schedule: the full season schedule
-    :type schedule: dict[str, list[dict]]
+    :param games: List of Games played in a week
+    :type games: list[Game]
     :return: dictionary of the average rank difference for weekly matchups
-    :rtype: dict[str, float]
+    :rtype: float
     """
 
-    week_diffs_dict = {}
-    for week_name, games in schedule.items():
-        week_diffs = [g["diff"] for g in games]
+    week_diffs = [g.rank_diff for g in games]
 
-        avg_week_diff = sum(week_diffs) / len(week_diffs)
-        week_diffs_dict[week_name] = avg_week_diff
+    avg_week_diff = sum(week_diffs) / len(week_diffs)
 
-    return week_diffs_dict
+    return avg_week_diff
